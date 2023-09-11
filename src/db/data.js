7b385cea72f60e0e7ff3cc16f5b4521c2a7730c8
builtbysuraj <surajgupta3553@gmail.com>
2023-09-11 12:03:19 +0530

    Add more house data

--- src/db/data.js
+++ src/db/data.js
@@ -62,8 +62,60 @@ export const data = [
   {
     "id": 9,
     "img": "https://images.pexels.com/photos/32870/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    "name": "Faulkner Ave",
-    "price": 7046,
+    "name": "Oceanfront Oasis",
+    "price": 7546,
+    "location": "Cherry Hill, Texas",
+  },
+  {
+    "id": 10,
+    "img": "https://images.pexels.com/photos/460695/pexels-photo-460695.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "name": "Prairie Palace",
+    "price": 2046,
     "location": "Green Valley",
+  },
+  {
+    "id": 11,
+    "img": "https://images.pexels.com/photos/534228/pexels-photo-534228.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "name": "Lakeside Loft",
+    "price": 6206,
+    "location": "North Springfield",
+    "popular": true
+  },
+  {
+    "id": 12,
+    "img": "https://images.pexels.com/photos/209274/pexels-photo-209274.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "name": "Desert Dunes Villa",
+    "price": 7331,
+    "location": "Birchwood, Wisconsin",
+  },
+  {
+    "id": 13,
+    "img": "https://images.pexels.com/photos/208736/pexels-photo-208736.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "name": "Downtown Brick Duplex",
+    "price": 2089,
+    "location": "Spruce Springs, Nevada",
+  },
+  {
+    "id": 14,
+    "img": "https://images.pexels.com/photos/3082638/pexels-photo-3082638.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "name": "Sunset Suburban Villa",
+    "price": 7459,
+    "location": "Cedar Springs, Colorado",
+    "popular": true
+  },
+  {
+    "id": 15,
+    "img": "https://images.pexels.com/photos/1105754/pexels-photo-1105754.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "name": "Woodland Waterfront Cottage",
+    "price": 4461,
+    "location": "Maple Grove, Oregon",
+  },
+  {
+    "id": 16,
+    "img": "https://images.pexels.com/photos/7031607/pexels-photo-7031607.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "name": "Garden Grove Manor",
+    "price": 9309,
+    "location": "Pine Hill, Arizona",
+    "popular": true
   },
 ]
